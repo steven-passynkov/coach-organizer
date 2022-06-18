458f0976ea8e465367758aa3764d7e5b086d9d32
Steven Passynkov <passynkovsteven@gmail.com>
2022-06-18 19:11:20 -0400

    settings and lessons

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "61fd21eb901c17a4"; }
+/******/ 		__webpack_require__.h = function() { return "461fb39bf53fdaf6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
